3b7456086766287019d3292992c0f0c599adfac2
wiklauri27giorgi <gtsiklauri27@gmail.com>
2025-01-14 19:55:26 +0400

    first commit

---
+++ day 01/cw/cw.py
@@ -0,0 +1,7 @@
+# შექმენით 9 ცვლადი და გამოიყენეთ სხვადასხვა მონაცემთა ტიპი, შემდეგ გვერძე მიუწერე ცვლადის მონაცემთა ტიპი, და 9 ცვლადის მეშვეობიტ დაწერე რაიმე დიდი წინადადებ=1.5
+name1="giorig" #str 
+age=12  #int
+cekva=1.5 #float
+pet="tobi" #str 
+sleep_time=12 #int
+print("my name is", name1, "my age is", age, "how many hours i trein at dance", cekva, "my pets name is", pet, "I sleep at", sleep_time)
